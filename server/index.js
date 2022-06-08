cc69eb2a0b61f38fc1d9d9004d778951e55e6428
Jonathan Slack <slack6000@gmail.com>
2022-06-08 11:40:50 -0500

    second save

--- server/index.js
+++ server/index.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
 
 
 
-const port = process.env.SERVER_PORT || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`listening on ${port}`))
